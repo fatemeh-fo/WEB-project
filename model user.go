ca70dab373faee55f5658725e110d6e22edf7e57
fatemeh-fo <63150708+fatemeh-fo@users.noreply.github.com>
2023-07-15 21:56:01 +0330

    Add files via upload

---
+++ model user.go
@@ -0,0 +1,14 @@
+// backend/models/user.go
+
+package models
+
+import "time"
+
+// User represents a user in the system.
+type User struct {
+	ID        int       `json:"id"`
+	Username  string    `json:"username"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
